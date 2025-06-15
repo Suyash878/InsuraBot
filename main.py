1a189c94b1677d1bce22472cb9ced0e78e0596cf
Suyash <suyashmishra145@gmail.com>
2025-06-16 02:27:33 +0530

    finally fixing issues

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import mindsdb_sdk
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+EMBEDDING_API_KEY = os.getenv('EMBEDDING_API_KEY')
+RERANKING_API_KEY = os.getenv('RERANKING_API_KEY')
+
 con = mindsdb_sdk.connect()
 con = mindsdb_sdk.connect('http://127.0.0.1:47334')
 
@@ -6,19 +15,19 @@ databases = con.databases.list()
 
 database = databases[0]
 
-query = con.query("""
-CREATE KNOWLEDGE_BASE text_case_kb
+query = con.query(f"""
+CREATE KNOWLEDGE_BASE random_case_kb
 USING
-    embedding_model = {
+    embedding_model = {{
         "provider": "gemini",
         "model_name" : "text-embedding-004",
-        "api_key": "AIzaSyBcRaIybCZm9R2aDsU-yePHdiHeblwO4BM"
-    },
-    reranking_model = {
+        "api_key": "{EMBEDDING_API_KEY}"
+    }},
+    reranking_model = {{
         "provider": "together_ai",
         "model_name": "Salesforce/Llama-Rank-V1",
-        "api_key": "4903bdc4bd98583e8158c62c0b0a496b0c5d4b7c8143fcca1821736ff335f7fa"
-    },
+        "api_key": "{RERANKING_API_KEY}"
+    }},
     metadata_columns = ['sales_rep'],
     content_columns = ['region'],
     id_column = 'transaction_id';""")
